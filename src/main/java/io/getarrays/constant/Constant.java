fc3fea51aa52b39df6d016378f5291a2bd4d5784
ertanAlabay <ertanosman758@gmail.com>
2024-10-03 23:03:53 +0300

    Second Commit for Spring Project

--- src/main/java/io/getarrays/constant/Constant.java
+++
@@ -1,5 +0,0 @@
-package io.getarrays.constant;
-
-public class Constant {
-    public static final String PHOTOT_DIRECTORY = System.getProperty("user.home") + "/Downloads/uploads";
-}
--- src/main/java/io/getarrays/contactapi/domain/Contact.java
+++ src/main/java/io/getarrays/contactapi/domain/Contact.java
@@ -1,23 +1,26 @@
-package io.getarrays.domain;
-
-import org.hibernate.annotations.UuidGenerator;
+package io.getarrays.contactapi.domain;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.UuidGenerator;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_DEFAULT;
+
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
+@JsonInclude(NON_DEFAULT)
 @Table(name = "contacts")
-@JsonInclude(JsonInclude.Include.NON_DEFAULT)
 public class Contact {
 	@Id
 	@UuidGenerator
